7e44c1898d6e3036ee398667662af65d55acd0ea
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-10-01 09:59:45 -0500

    trabajando con el curso basico de java

---
+++ src/Desafio.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        String nombre = "Ivan Arturo Diaz Rosso";
+        String tipoCuenta = "Ahorro";
+        double saldo = 532000;
+        int opcion= 0;
+        float cantidadRetiro=0;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("************************************* \n");
+        System.out.println("Nombre del cliente: " + nombre);
+        System.out.println("Tipo de cuenta: " + tipoCuenta);
+//        System.out.println("Saldo disponible: $ " + saldo);
+        while (opcion != 9){
+            System.out.println("\n*************************************");
+            System.out.println("** Escribe el numero con la opcion deseada: **");
+            System.out.println("1 - Consultar saldo");
+            System.out.println("2 - Retirar");
+            System.out.println("3 - Depositar");
+            System.out.println("9 - Salir");
+            System.out.print("Opcion: ");
+            opcion = teclado.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Tu saldo actual es: " + saldo);
+
+                    break;
+                case 2:
+                    System.out.println("Cual es el valor que deseas retirar? ");
+                    cantidadRetiro = teclado.nextFloat();
+                    if (cantidadRetiro> saldo){
+                        System.out.println("Saldo insufieciente");
+                    }else if(cantidadRetiro < saldo){
+                        saldo = saldo - cantidadRetiro;
+                        System.out.println("Operacion exitosa");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Cual es el valor que deseas consignar? ");
+                    cantidadRetiro = teclado.nextFloat();
+                    saldo += cantidadRetiro;
+                    break;
+                case 9:
+                    System.out.println("Finalizando el programa. Muchas gracias por usar nuestros servicios");
+                    break;
+                default:
+                    break;
+            }
+
+
+
+        }
+
+    }
+}
+
+
+
